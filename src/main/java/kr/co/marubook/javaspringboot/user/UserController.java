03468f791354ecd0f9142ce397cf0ea9b3ebed2f
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-05-30 21:56:39 +0900

    User Api: Status Code ResponseEntity 통해 전달

--- src/main/java/kr/co/marubook/javaspringboot/user/UserController.java
+++ src/main/java/kr/co/marubook/javaspringboot/user/UserController.java
@@ -1,7 +1,10 @@
 package kr.co.marubook.javaspringboot.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 // GET /users -> 모든 User들
@@ -25,7 +28,15 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public User registerUser(@RequestBody User user) {
-        return service.save(user);
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User newUser = service.save(user);
+        // URI는 자원의 저장 장소, 즉 End Point를 가리킴
+        // 이는 Header를 통해 내려온다
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("{id}")
+                .buildAndExpand(newUser.getId())
+                .toUri();
+        // ResponseEntity.created는 201 status code와 함께 전달된다.
+        return ResponseEntity.created(location).build();
     }
 }
