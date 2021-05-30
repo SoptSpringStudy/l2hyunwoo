0164b7e8174ea2aed926b3e6a2449c43d5173ca9
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-05-30 13:33:26 +0900

    GET: Path Variable을 활용한 가변적인 REST GET

--- src/main/java/kr/co/marubook/javaspringboot/helloworld/HelloWorldController.java
+++ src/main/java/kr/co/marubook/javaspringboot/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package kr.co.marubook.javaspringboot.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +14,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldDto getHelloWorldDto() {
         return new HelloWorldDto("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path/{name}")
+    public HelloWorldDto getHelloWorldWithName(@PathVariable String name) {
+        return new HelloWorldDto(String.format("Hello, %s", name));
     }
 }
