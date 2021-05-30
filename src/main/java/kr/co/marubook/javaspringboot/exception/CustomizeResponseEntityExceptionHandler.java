fe6d74962ba7932ad58d1581641664323c5e33dc
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-05-31 01:02:40 +0900

    User Api: Error handling with Spring AOP

--- src/main/java/kr/co/marubook/javaspringboot/exception/CustomizeResponseEntityExceptionHandler.java
+++ src/main/java/kr/co/marubook/javaspringboot/exception/CustomizeResponseEntityExceptionHandler.java
@@ -1,2 +1,31 @@
-package kr.co.marubook.javaspringboot.exception;public class CustomizeResponseEntityExceptionHandler {
+package kr.co.marubook.javaspringboot.exception;
+
+import kr.co.marubook.javaspringboot.user.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+// @ControllerAdvice: 모든 Controller가 시작할 때 해당 Bean이 시작되도록 강제
+@RestController
+@ControllerAdvice
+public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptions(Exception exception, WebRequest request) {
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), exception.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleUserNotFoundException(Exception exception, WebRequest request) {
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), exception.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
 }
--- src/main/java/kr/co/marubook/javaspringboot/exception/ExceptionResponse.java
+++ src/main/java/kr/co/marubook/javaspringboot/exception/ExceptionResponse.java
@@ -1,2 +1,16 @@
-package kr.co.marubook.javaspringboot.exception;public class ExceptionResponse {
+package kr.co.marubook.javaspringboot.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ExceptionResponse {
+    private Date timeStamp;
+    private String message;
+    private String details;
 }
