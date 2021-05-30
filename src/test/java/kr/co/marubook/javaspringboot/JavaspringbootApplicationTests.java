9eb6b097c19a77456a2abbffc4cddd5b58701487
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-05-30 12:43:03 +0900

    INIT COMMIT

---
+++ src/test/java/kr/co/marubook/javaspringboot/JavaspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.marubook.javaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
